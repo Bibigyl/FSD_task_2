2cb2bba92fa070c6f768e4e496298f4dcca516ed
Kate <ekaterina.kryakushina@yandex.ru>
2019-07-29 01:16:12 +0300

    pagination base added

--- src/js/main.js
+++ src/js/main.js
@@ -7,79 +7,115 @@ import "../libs/jquery-ui-1.12.1.custom/jquery-ui.min.js";
 
 $( document ).ready(function() {
   
-  $(".input_masked_date").mask('99/99/9999',{placeholder:"ДД/ММ/ГГГГ"});
+$(".input_masked_date").mask('99/99/9999',{placeholder:"ДД/ММ/ГГГГ"});
 
 
 
 
-$(function() {
+//-----------------Slider--------------------------
 
-  // Initiate Slider
-  $('#slider-range').slider({
-    range: true,
-    min: 10000,
-    max: 110000,
-    step: 100,
-    values: [45000, 75000]
-  });
+// Initiate Slider
+$('#slider-range').slider({
+  range: true,
+  min: 200,
+  max: 15500,
+  step: 200,
+  values: [5000, 10000]
+});
 
-  // Move the range wrapper into the generated divs
-  $('.ui-slider-range').append($('.range-wrapper'));
+// Apply initial values to the range container
+$('.range').html('<span class="range-value">' + $('#slider-range').slider("values", 0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span><span class="range-divider"></span><span class="range-value">' + $("#slider-range").slider("values", 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span>');
 
-  // Apply initial values to the range container
-  $('.range').html('<span class="range-value">' + $('#slider-range').slider("values", 0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '<sup>$</sup></span><span class="range-divider"></span><span class="range-value">' + $("#slider-range").slider("values", 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '<sup>$</sup></span>');
+$('#slider-range').slider({
+  slide: function(event, ui) {
 
-  // Show the gears on press of the handles
-  $('.ui-slider-handle, .ui-slider-range').on('mousedown', function() {
-    $('.gear-large').addClass('active');
-  });
+    // Update the range container values upon sliding
 
-  // Hide the gears when the mouse is released
-  // Done on document just incase the user hovers off of the handle
-  $(document).on('mouseup', function() {
-    if ($('.gear-large').hasClass('active')) {
-      $('.gear-large').removeClass('active');
-    }
-  });
+    $('.range').html('<span class="range-value">' + ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span><span class="range-divider"></span><span class="range-value">' + ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span>');
 
-  // Rotate the gears
-  var rangeWidth = $('.ui-slider-range').css('width');
+    // Get old value
+    var previousVal = parseInt($(this).data('value'));
 
-  $('#slider-range').slider({
-    slide: function(event, ui) {
+    // Save new value
+    $(this).data({
+      'value': parseInt(ui.value)
+    });
 
-      // Update the range container values upon sliding
+  }
 
-      $('.range').html('<span class="range-value"><sup>$</sup>' + ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span><span class="range-divider"></span><span class="range-value"><sup>$</sup>' + ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '</span>');
-
-      // Get old value
-      var previousVal = parseInt($(this).data('value'));
-
-      // Save new value
-      $(this).data({
-        'value': parseInt(ui.value)
-      });
+});
 
 
-      if (ui.values[1] === 110000) {
-        if (!$('.range-alert').hasClass('active')) {
-          $('.range-alert').addClass('active');
+//---------------------------Custom pagination -------------------------------
+
+
+
+
+});
+
+//---------------------------Pagination -------------------------------
+
+var totalElements = 300; // elements to show on the page
+var perPage = 20; // how many elements need to show per page
+var visibleLinks = 4; // how many links need to be visible;
+
+var linksCount = Math.ceil(totalElements / perPage); // pagination links count;
+var paginationContainer = $(".pagination");
+var paginationBody = $(".paginationBody"); // where the pagination will be append
+
+// create the first array of links
+const paginationArray = [...Array(linksCount > visibleLinks ? visibleLinks + 1 : linksCount).keys()].slice(1); 
+paginationArray.splice(visibleLinks - 1, 1, linksCount);
+
+// Pagination init function
+(function paginationInit(currentPage = 1) {
+  if (linksCount > visibleLinks) {
+    if ( linksCount - currentPage < visibleLinks - 1 && currentPage !== paginationArray[1] ) {
+      if (linksCount - currentPage >= visibleLinks - 2) {
+        currentPage = currentPage - 1;
+      } else if(currentPage !== paginationArray[paginationArray.length - 2] && 
+                currentPage !== paginationArray[paginationArray.length - 1]){
+        return;        
+      }else {
+        currentPage = currentPage - 1 - (visibleLinks - 2 - (linksCount - currentPage));
+      }
+    } else if (currentPage !== 1) {
+      if (
+        currentPage > paginationArray[1] &&
+        currentPage < paginationArray[paginationArray.length - 2]
+      ) {
+        return;
+      } else if ( currentPage === paginationArray[1] ){
+        if( currentPage - (visibleLinks - 2) >= -1 ) {
+          currentPage = currentPage - (visibleLinks - 2) >= 2 ? currentPage - (visibleLinks - 2) : 1;
+        }else{
+          currentPage = 1;
         }
       } else {
-        if ($('.range-alert').hasClass('active')) {
-          $('.range-alert').removeClass('active');
-        }
+        currentPage -= 1;
       }
+    } else {
+      currentPage = 1;
     }
+    paginationArray.splice(1, paginationArray.length - 2,...Array(visibleLinks - 2)
+        .fill(currentPage)
+        .map((e, i) => (e += i + 1))
+    );
+  }
+  
+  paginationContainer.html("");
+  
+  paginationArray.map(e => {
+    paginationContainer.append("<a href=" + e + ">" + e + "</a>");
   });
-
-  // Prevent the range container from moving the slider
-  $('.range, .range-alert').on('mousedown', function(event) {
-    event.stopPropagation();
+  if(paginationArray[1] > 2) {
+    $(paginationContainer.children("a")[1]).before("<span>...</span>");
+  }
+  if(paginationArray[paginationArray.length - 2] < linksCount - 1) {
+    $(paginationContainer.children("a")[paginationArray.length - 1]).before('<span>...</span>')
+  }
+  paginationContainer.children("a").on("click", function(e) {
+    e.preventDefault();
+    paginationInit(+$(e.target).attr("href"));
   });
-
-});
-
-
-});
-
+})();
