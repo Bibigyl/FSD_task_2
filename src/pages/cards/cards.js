040d589ea2f347cddacf49f5765e82b5bc49098a
Kate <ekaterina.kryakushina@yandex.ru>
2019-12-19 02:13:43 +0300

    Pages Form elements, Cards. Before adding datepicker #3

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,3 +2,34 @@
 
 import '../../scss/main.scss';
 import './cards.scss';
+
+//--------------- import libs -----------------------------
+
+//import "../../libs/daterangepicker-master/moment.min.js";
+//import "../../libs/daterangepicker-master/daterangepicker.js";
+//import "../../libs/jquery.daterangepicker/jquery.daterangepicker.js";
+
+
+import moment from 'moment'
+import 'moment/locale/ru' 
+moment.locale('ru');
+
+//--------------- import common js -----------------------------
+
+import "../../js/main.js";
+
+//------------------ import blocks scripts -----------------------------
+
+import "../../common.blocks/text-field/text-field.js";
+//import "../../common.blocks/slider-range/slider-range.js";
+import "../../common.blocks/dropdown/dropdown.js";
+import {setDropdownValues} from "../../common.blocks/dropdown/dropdown.js";
+//import "../../common.blocks/expandable-checkbox-list/expandable-checkbox-list.js";
+//import "../../common.blocks/pagination/pagination.js";
+//import "../../common.blocks/like-button/like-button.js";
+
+//------------------ import components scripts -----------------------------
+
+//import "../../components/booking-card/booking-card.js";
+//import "../../components/search-card/search-card.js";
+//import "../../components/header/header.js";
