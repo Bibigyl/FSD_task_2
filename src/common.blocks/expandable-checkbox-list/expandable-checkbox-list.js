b756a82b7b3d3690b38488a2e4fc02476d67486b
Kate <ekaterina.kryakushina@yandex.ru>
2019-12-26 18:39:43 +0300

    Joined checkbox and rich checkbox. Changed expandable checkbox list #3

--- src/common.blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/common.blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $(".expandable-checkbox-list__arrow").on("click", function() {
     $(this)
-    .parent().find('.checkbox-buttons').slideToggle(400)
+    .parent().find('.expandable-checkbox-list__list').slideToggle(400)
     .parent().toggleClass("expandable-checkbox-list_open");
   });
 });
