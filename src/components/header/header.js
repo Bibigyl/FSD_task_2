6d825450e066f5defb8196744fae023c84100492
Kate <ekaterina.kryakushina@yandex.ru>
2019-12-29 23:22:30 +0300

    Header #3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,33 +1,12 @@
-//Checks all checkboxs triggering all CSS transitions 
-function check (status) {
-  $("#menu__button").checked = status;
+$(document).ready(function () {
 
-  if(status){
-    var uncheck = setTimeout(function() { 
-      check(false);
-    }, 2000);
-  }
-  
-}
+  $('.header__hamburger').each(function () {
 
+    $(this).on('click', function () {
+      $(this).closest('.header').find('.header__menu').slideToggle(400);
+      $(this).toggleClass("header__hamburger_active");
+    });
 
-// Triggers demonstration on button click
-$("#menu__button").onclick = function () {
-  check(true);
-};
-
-// Flashes menus on load
-document.addEventListener("DOMContentLoaded", function(event) { 
-  check(true);
-});
-
-$("#menu__button").on("click", function() {
-	$(this).parent().children(".mobil-menu").slideToggle(300, "linear").css("display", "flex");
-})
-
-
-$(document).ready(function(){
-  $(".header__hamburger").click(function(){
-    $(this).toggleClass("header__hamburger_active");
   });
+
 });
