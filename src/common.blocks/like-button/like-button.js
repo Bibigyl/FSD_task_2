7ffcb19aa7616a95f249a447e31b9e2a002285bd
Kate <ekaterina.kryakushina@yandex.ru>
2020-01-04 00:22:04 +0300

    Like button, js

--- src/common.blocks/like-button/like-button.js
+++ src/common.blocks/like-button/like-button.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	
-	$(".like-button__input").on("change", function() {
-		$(this).closest('.loke-button').toggleClass("like-button_checked");
+	$(".js-like-button__input").on("change", function() {
+		let $btn = $(this).closest('.like-button');
+		let $btnNum = $btn.find('.js-like-button__number')
+		let num = +$btnNum.text();
+
+		$btn.toggleClass("like-button_checked");
+		$btn.hasClass('like-button_checked') ? num++ : num--;
+		$btnNum.text(num);
 	});
 });
