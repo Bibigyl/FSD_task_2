25e5f83146069b49030312d9601ba9e78c8e84ce
Kate <ekaterina.kryakushina@yandex.ru>
2020-01-04 21:02:58 +0300

    Search room #3

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -5,12 +5,8 @@ import './search-room.scss';
 
 //--------------- import libs -----------------------------
 
-//import "../../libs/jquery.maskedinput/jquery.maskedinput.min.js";
 import "../../libs/jquery-ui-1.12.1.custom/jquery-ui.min.js";
-//import "../../libs/daterangepicker-master/moment.min.js";
-//import "../../libs/daterangepicker-master/daterangepicker.js";
-//import "../../libs/jquery.daterangepicker/jquery.daterangepicker.js";
-
+import "../../libs/daterangepicker/jquery.daterangepicker.js";
 
 import moment from 'moment'
 import 'moment/locale/ru' 
@@ -22,58 +18,13 @@ import "../../js/main.js";
 
 //------------------ import blocks scripts -----------------------------
 
-//import "../../common.blocks/text-field/text-field.js";
-//import "../../common.blocks/slider-range/slider-range.js";
-//import "../../common.blocks/dropdown/dropdown.js";
-//import "../../common.blocks/expandable-checkbox-list/expandable-checkbox-list.js";
-//import "../../common.blocks/pagination/pagination.js";
+import "../../common.blocks/text-field/text-field.js";
+import "../../common.blocks/slider-range/slider-range.js";
+import "../../common.blocks/dropdown/dropdown.js";
+import "../../common.blocks/filter-date-dropdown/filter-date-dropdown.js";
+import "../../common.blocks/expandable-checkbox-list/expandable-checkbox-list.js";
+import "../../common.blocks/pagination/pagination.js";
 
 //------------------ import components scripts -----------------------------
 
-/* import "../../components/header/header.js";
-
-$( document ).ready(function() {
-
-  $('#search-room__date-search').dateRangePicker({
-    language: 'ru',
-    singleMonth: true,
-    showShortcuts: false,
-    showTopbar: true,
-    startOfWeek: 'monday',
-    format: 'D MMM',
-    separator: ' - ',
-    hoveringTooltip: false,
-    customArrowPrevSymbol: '<div class="calendar__arrow arrow"><i class="material-icons">arrow_back</i></div>',
-    customArrowNextSymbol: '<div class="calendar__arrow arrow"><i class="material-icons">arrow_forward</i></div>',
-  }).bind('datepicker-opened', function() {
-
-    var dayClickCounter = 0;
-
-    $(".month-wrapper .day").on("click", function() {
-      dayClickCounter += 1;
-      if ( dayClickCounter == 1 ) {
-        $(this).parent().addClass("td-first-day-selected-temp");
-      } else if ( dayClickCounter == 2 ) {
-        $(this).parent().parent().parent().find(".td-first-day-selected-temp").removeClass("td-first-day-selected-temp");
-        dayClickCounter = 0;
-      }
-    });
-
-    $('.calendar__link_clear').click(function(evt)
-    {
-      evt.stopPropagation();
-      $('#search-room__date-search').data('dateRangePicker').clear();
-      $(".td-checked").removeClass("td-checked td-first-day-selected-temp");
-    });
-
-    $('.calendar__link_apply').click(function(evt)
-    {
-      evt.stopPropagation();
-      $('#search-room__date-search').data('dateRangePicker').close();
-    });
-
-  });
-
-  $('#search-room__date-search').data('dateRangePicker').setDateRange('19 авг.','23 авг.');
-
-}); */
+import "../../components/header/header.js";
