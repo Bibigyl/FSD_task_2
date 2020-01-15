133eb8233db7b9b891443e7c1bbbf32af6bbf1e6
Kate <ekaterina.kryakushina@yandex.ru>
2020-01-15 20:18:29 +0300

    Dropdown, js-classes #3

--- src/common.blocks/dropdown/dropdown.js
+++ src/common.blocks/dropdown/dropdown.js
@@ -1,69 +1,69 @@
 $( document ).ready(function() {
 
-    $(".dropdown__arrow").each(function() {
+    $(".js-dropdown__arrow").each(function() {
         $(this).on("click", function() {
-            let $dropdown = $(this).closest('.dropdown');
+            let $dropdown = $(this).closest('.js-dropdown');
 
-            $dropdown.children(".dropdown__list").slideToggle(200, "linear");$dropdown.toggleClass("dropdown_open");
+            $dropdown.children(".js-dropdown__list").slideToggle(200, "linear");$dropdown.toggleClass("dropdown_open");
         });
     });
 
 
-    $(".dropdown__button_action_clear").each(function() {
+    $(".js-dropdown__button_action_clear").each(function() {
         $(this).on("click", function() {
-            let $dropdown = $(this).closest('.dropdown');
+            let $dropdown = $(this).closest('.js-dropdown');
             let initialText = $dropdown.attr('data-initial-text');
-            let $list = $dropdown.find(".dropdown__list");
-            let $liArr = $list.find('li');
+            let $list = $dropdown.find(".js-dropdown__list");
+            let $liArr = $list.find('.js-dropdown__item');
 
             for ( let i = 0; i < $liArr.length; i++ ) {
-                $liArr.eq(i).children(".dropdown__number").text("0");
-                $liArr.eq(i).children(".dropdown__button_action_subtrack").addClass("dropdown__button_not-active"); 
+                $liArr.eq(i).children(".js-dropdown__number").text("0");
+                $liArr.eq(i).children(".js-dropdown__button_action_subtrack").addClass("dropdown__button_not-active"); 
             }
 
-            $dropdown.find(".dropdown__field span").text(initialText)
+            $dropdown.find(".js-dropdown__text").text(initialText)
             $(this).addClass("dropdown__button_hidden");
         });
     });
 
 
     
-    $(".dropdown__button_action_apply").each(function() {
+    $(".js-dropdown__button_action_apply").each(function() {
         $(this).on("click", function() {
-            let $dropdown = $(this).closest('.dropdown');
+            let $dropdown = $(this).closest('.js-dropdown');
 
-            $dropdown.find('.dropdown__list').slideToggle(200, "linear").parent().toggleClass("dropdown_open");
+            $dropdown.find('.js-dropdown__list').slideToggle(200, "linear").parent().toggleClass("dropdown_open");
         });
     })
 
 
 
-    $(".dropdown__button_to-change-quantity").each(function() {
+    $(".js-dropdown__button_to-change-quantity").each(function() {
         $(this).on("click", function() {
 
-            let $dropdown = $(this).closest('.dropdown');
+            let $dropdown = $(this).closest('.js-dropdown');
             let initialText = $dropdown.attr('data-initial-text');
-            let $textField = $dropdown.find(".dropdown__text");
+            let $textField = $dropdown.find(".js-dropdown__text");
             let count = 0;
-            let $numNode = $(this).parent().find(".dropdown__number");
+            let $numNode = $(this).parent().find(".js-dropdown__number");
             let num = $numNode.text() * 1;
-            let $list = $dropdown.find(".dropdown__list");
-            let $ul = $list.children('ul');
+            let $list = $dropdown.find(".js-dropdown__list");
+            let $ul = $list.children('.js-dropdown__items');
     
             if ( !$(this).hasClass("dropdown__button_not-active") ) {
-                if ( $(this).hasClass("dropdown__button_action_subtrack") ) {
+                if ( $(this).hasClass("js-dropdown__button_action_subtrack") ) {
                     num = num - 1;
                     $numNode.text(num);
                     if ( num == 0 ) { $(this).addClass("dropdown__button_not-active") }
                     if ( num == 98 ) { 
-                        $(this).parent().find(".dropdown__button_action_add").removeClass("dropdown__button_not-active"); 
+                        $(this).parent().find(".js-dropdown__button_action_add").removeClass("dropdown__button_not-active"); 
                     }
                 } else {
                     num = num + 1;
                     $numNode.text(num);
                     if ( num == 99 ) { $(this).addClass("dropdown__button_not-active") }
                     if ( num == 1 ) { 
-                        $(this).parent().find(".dropdown__button_action_subtrack").removeClass("dropdown__button_not-active"); 
+                        $(this).parent().find(".js-dropdown__button_action_subtrack").removeClass("dropdown__button_not-active"); 
                     }
                 }
             } else {
@@ -71,13 +71,13 @@ $( document ).ready(function() {
             }
     
             if ( $(this).hasClass("dropdown__button_action_add") ) {
-                $dropdown.find(".dropdown__button_action_clear").removeClass("dropdown__button_hidden");
+                $dropdown.find(".js-dropdown__button_action_clear").removeClass("dropdown__button_hidden");
             } else {
                 for ( let i = 0; i < $ul.children().length; i++ ) {
                     count = count + $ul.children().eq(i).find(".dropdown__number").text()*1;
                 }
                 if ( count == 0 ) {
-                    $dropdown.find(".dropdown__button_action_clear").addClass("dropdown__button_hidden");
+                    $dropdown.find(".js-dropdown__button_action_clear").addClass("dropdown__button_hidden");
                 }
             }
     
@@ -88,10 +88,10 @@ $( document ).ready(function() {
                 let $li;
                 let newText = '';
     
-                $ul.children(".dropdown__item").each(function() {
+                $ul.children(".js-dropdown__item").each(function() {
                     $li = $(this);
-                    if ($li.find('.dropdown__number').text() != '0') {
-                        newText = newText + $li.find('.dropdown__number').text() + ' ' + $li.attr('item') + ', ';
+                    if ($li.find('.js-dropdown__number').text() != '0') {
+                        newText = newText + $li.find('.js-dropdown__number').text() + ' ' + $li.attr('item') + ', ';
                     }
                 });
     
@@ -103,11 +103,11 @@ $( document ).ready(function() {
 });
 
 function setDropdownValues($node, vals) {
-    let $items = $node.find('.dropdown__item');
+    let $items = $node.find('.js-dropdown__item');
 
     vals.forEach(function(item, i) {
         for (let r = 1; r <= +item; r++) {
-            $items.eq(i).find('.dropdown__button_action_add').trigger('click');
+            $items.eq(i).find('.js-dropdown__button_action_add').trigger('click');
         }        
     })
 }
