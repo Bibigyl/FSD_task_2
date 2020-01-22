65dcbe18519aa70f969f8848d8b8296b672028e2
Kate <ekaterina.kryakushina@yandex.ru>
2020-01-22 21:49:34 +0300

    Dropdowns #3

--- src/blocks-base/dropdown/dropdown.js
+++ src/blocks-base/dropdown/dropdown.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    $(".js-dropdown__arrow").each(function() {
-        $(this).on("click", function() {
+    $(".js-dropdown__arrow").each(function () {
+        $(this).on("click", function () {
             let $dropdown = $(this).closest('.js-dropdown');
 
             $dropdown.children(".js-dropdown__popup").slideToggle(200, "linear");
@@ -10,12 +10,12 @@ $( document ).ready(function() {
     });
 
 
-    $(".js-dropdown__action_clear").each(function() {
-        $(this).on("click", function() {
+    $(".js-dropdown__action_clear").each(function () {
+        $(this).on("click", function () {
             let $dropdown = $(this).closest('.js-dropdown');
             let $items = $dropdown.find('.js-dropdown__item-data');
 
-            $items.each(function() {
+            $items.each(function () {
                 $(this).attr('data-clear', true);
             });
 
@@ -23,9 +23,9 @@ $( document ).ready(function() {
         });
     });
 
-    
-    $(".js-dropdown__action_apply").each(function() {
-        $(this).on("click", function() {
+
+    $(".js-dropdown__action_apply").each(function () {
+        $(this).on("click", function () {
             let $dropdown = $(this).closest('.js-dropdown');
 
             $dropdown.find('.js-dropdown__popup').slideToggle(200, "linear").parent().toggleClass("dropdown_open");
@@ -33,7 +33,7 @@ $( document ).ready(function() {
     });
 
 
-    document.querySelectorAll('.js-dropdown').forEach(function(dropdown) {
+    document.querySelectorAll('.js-dropdown').forEach(function (dropdown) {
 
         let items = dropdown.querySelectorAll('.js-dropdown__item-data');
         let textField = dropdown.querySelector('.js-dropdown__text');
@@ -41,19 +41,22 @@ $( document ).ready(function() {
         let initialText = dropdown.getAttribute('data-initial-text');
         let text = '';
 
-        let dropdownObserver = new MutationObserver(function() {
-            
+
+
+        let dropdownObserver = new MutationObserver(function () {
+
             let itemText;
-            items.forEach(function(item) {
+            items.forEach(function (item) {
                 itemText = item.getAttribute('data-counted-item');
                 text = itemText != '' ? text + itemText + ', ' : text;
-            })
+            });
 
-            if ( text.slice(0, -2) ) {
-                clear.classList.remove('dropdown__action_hidden');
+
+            if (text.slice(0, -2)) {
+                if (clear) {clear.classList.remove('dropdown__action_hidden')};
                 text = text.slice(0, -2);
             } else {
-                clear.classList.add('dropdown__action_hidden');
+                if (clear) {clear.classList.add('dropdown__action_hidden')};
                 text = initialText;
             }
 
@@ -61,8 +64,11 @@ $( document ).ready(function() {
             text = '';
         });
 
-        items.forEach(function(item) {
-            dropdownObserver.observe(item, {attributes: true});
-        });   
+        items.forEach(function (item) {
+            dropdownObserver.observe(item, {
+                attributes: true
+            });
+        });
+
     });
 });
