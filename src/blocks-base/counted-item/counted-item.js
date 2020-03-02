4b3293a0a24bfa94cea94d4befff93aae53b1ed5
Kate <ekaterina.kryakushina@yandex.ru>
2020-03-03 02:17:37 +0300

    Deleted 0, 99 in counted-item. Changed *1 to parseInt

--- src/blocks-base/counted-item/counted-item.js
+++ src/blocks-base/counted-item/counted-item.js
@@ -4,26 +4,35 @@ $( document ).ready(function() {
         $(this).on("click", function() {
 
             let $item = $(this).closest('.js-counted-item');
-            let $valueNode = $item.find(".js-counted-item__value");
-            let value = $valueNode.text() * 1;
+            let $value = $item.find(".js-counted-item__value");
+            let $inc = $item.find(".js-counted-item__operation_inc");
+            let $dec = $item.find(".js-counted-item__operation_dec");
+            let value = parseInt( $value.text() );
             let text = $item.find(".js-counted-item__text").text();
+            let min = $item.attr('data-min');
+            let max = $item.attr('data-max');
     
             if ( !$(this).hasClass("counted-item__operation_disabled") ) {
+
                 if ( $(this).hasClass("js-counted-item__operation_dec") ) {
+
                     value = value - 1;
-                    $valueNode.text(value);
-                    if ( value == 0 ) { $(this).addClass("counted-item__operation_disabled") }
-                    if ( value == 98 ) { 
-                        $(this).parent().find(".js-counted-item__operation_inc").removeClass("counted-item__operation_disabled"); 
-                    }
+                    $inc.removeClass("counted-item__operation_disabled");
+
+                    if ( value == min ) { 
+                        $(this).addClass("counted-item__operation_disabled") 
+                    } 
                 } else {
+
                     value = value + 1;
-                    $valueNode.text(value);
-                    if ( value == 99 ) { $(this).addClass("counted-item__operation_disabled") }
-                    if ( value == 1 ) { 
-                        $(this).parent().find(".js-counted-item__operation_dec").removeClass("counted-item__operation_disabled"); 
+                    $dec.removeClass("counted-item__operation_disabled");
+
+                    if ( value == max ) { 
+                        $(this).addClass("counted-item__operation_disabled") 
                     }
                 }
+                $value.text(value);
+
             } else {
                 return;
             }
