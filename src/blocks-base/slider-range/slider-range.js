567bab23652232312c7fb73d564441a61b28a2d9
Kate <ekaterina.kryakushina@yandex.ru>
2020-03-14 17:05:40 +0300

    Regular expression in slider-range #6

--- src/blocks-base/slider-range/slider-range.js
+++ src/blocks-base/slider-range/slider-range.js
@@ -34,14 +34,27 @@ class SliderRange {
         });
 
         // Apply initial values to the range container
-        $text.html('<span class="range-value">' + $slider.slider("values", 0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span><span class="range-divider"></span><span class="range-value">' + $slider.slider("values", 1).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span>');
+        const regularExpression = /(\d)(?=(\d\d\d)+(?!\d))/g;
+        let begin = $slider.slider("values", 0).toString().replace(regularExpression, "$1 ");
+        let end = $slider.slider("values", 1).toString().replace(regularExpression, "$1 ");
+
+        $text.html(
+            `<span class="range-value">${begin}₽</span>
+            <span class="range-divider"></span>
+            <span class="range-value">${end}₽</span>`
+        );
 
         $slider.slider({
             slide: function(event, ui) {
 
+                let begin = ui.values[0].toString().replace(regularExpression, "$1 ");
+                let end = ui.values[1].toString().replace(regularExpression, "$1 ");
+                
                 // Update the range container values upon sliding
-                $text.html('<span class="range-value">' + ui.values[0].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span><span class="range-divider"></span><span class="range-value">' + ui.values[1].toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '<sup>₽</sup></span>');
-
+                $text.html(
+                    `<span class="range-value">${begin}₽</span>
+                    <span class="range-divider">
+                    </span><span class="range-value">${end}₽</span>`);
 
                 // Save new value
                 $slider.data({
